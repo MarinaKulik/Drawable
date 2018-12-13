543625342eebfd7aefa1f66df9f4c837647bc654
MarinaKulik <kulikmarina@list.ru>
2018-12-13 17:14:01 +0300

    Initial commit this is a first commit

---
+++ app/src/main/java/com/kulikmarina/drawable/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kulikmarina.drawable;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable wifi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.image);
+        imageView.setBackgroundResource(R.drawable.animation);
+
+        wifi = (AnimationDrawable) imageView.getBackground();
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        wifi.start();
+    }
+}
