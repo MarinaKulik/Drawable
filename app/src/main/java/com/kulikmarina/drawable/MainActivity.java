caeca573e14394346750b996e4824f2c84bcc12f
MarinaKulik <kulikmarina@list.ru>
2018-12-15 11:08:27 +0300

    update files

--- app/src/main/java/com/kulikmarina/drawable/MainActivity.java
+++ app/src/main/java/com/kulikmarina/drawable/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+//update
     AnimationDrawable wifi;
 
     @Override
