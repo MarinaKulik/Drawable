19a9bc7d31a42010fb717c0375b9519f8c561f74
MarinaKulik <kulikmarina@list.ru>
2018-12-15 11:45:23 +0300

    add background color

--- app/src/main/java/com/kulikmarina/drawable/MainActivity.java
+++ app/src/main/java/com/kulikmarina/drawable/MainActivity.java
@@ -3,11 +3,13 @@ package com.kulikmarina.drawable;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     AnimationDrawable wf;
+    View mView;
 
 
     @Override
