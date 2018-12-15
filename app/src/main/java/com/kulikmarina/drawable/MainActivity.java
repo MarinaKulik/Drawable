89b50f6e423292f2216c3e473f484d626954a641
MarinaKulik <kulikmarina@list.ru>
2018-12-15 11:30:51 +0300

    there rg

--- app/src/main/java/com/kulikmarina/drawable/MainActivity.java
+++ app/src/main/java/com/kulikmarina/drawable/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.image);
         imageView.setBackgroundResource(R.drawable.animation);
-
+//ho
         wifi = (AnimationDrawable) imageView.getBackground();
 
     }
